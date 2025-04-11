03f9789e404a3592d8c6ab3cebb9ef599224fa40
Logan Alleyne <logan.alleyne@outlook.com>
2025-04-11 14:29:07 +0100

    Update Pagination.vue

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-space-between items-center py-4 px-2">
         <button 
-            class="w-22 bg-blue-500 text-white p-2 py-2 rounded" 
+            class="w-22 bg-blue-500 text-white p-2 py-2 rounded hover:bg-blue-600 transition duration-200" 
             :class="{ invisible: !hasPrevPage }"
             :disabled="!hasPrevPage"
             @click="prevPage"
@@ -12,7 +12,7 @@
             <span class="text-gray-700">Page {{ page + 1 }} of {{ totalPages }}</span>
         </div>
         <button 
-            class="w-22 bg-blue-500 text-white px-4 py-2 rounded"
+            class="w-22 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200"
             :class="{ invisible: !hasNextPage }"
             :disabled="!hasNextPage"
             @click="nextPage"
